eb858863e28d2624bd3e06c42a0692b25429a8d8
Tanniesita <tannia.elizondo@ucrso.info>
2016-09-24 15:40:37 -0600

    Add files via upload

---
+++ Laboratorio2/src/UnidadesDeMedida.java
@@ -0,0 +1,27 @@
+package empresa;
+
+/**
+ *
+ * @author Tannia
+ */
+public enum UnidadesDeMedida {
+    KILOGRAMO("Kilogramo", 1000),
+    GRAMO("Gramo", 1),
+    MILIGRAMO("Miligramo", 0.001);
+
+    private final String UNIDAD;
+    private final double VALOR;
+
+    private UnidadesDeMedida(String UNIDAD, double VALOR) {
+        this.UNIDAD = UNIDAD;
+        this.VALOR = VALOR;
+    }
+
+    public String getUNIDAD() {
+        return UNIDAD;
+    }
+
+    public double getVALOR() {
+        return VALOR;
+    }
+}
