8b41feadbd75d1057b8be84bc37e3492ab9fd0bf
mario_000 <mario_000@192.168.1.6>
2016-09-24 02:19:21 -0600

    Cambios Varios en comentarios

--- Laboratorio2/src/Empresa.java
+++ Laboratorio2/src/Empresa.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package empresa;
-
 /**
  *
  * @author Tannia
  * @author Juan Diego 
- * @author Mario
+ * @author Mario Carranza Mena B51573
  */
 public abstract class Empresa {
     
--- Laboratorio2/src/ManejadorServicios.java
+++ Laboratorio2/src/ManejadorServicios.java
@@ -1,5 +1,3 @@
-package empresa;
-
 /**
  *
  * @author Tannia
